1661b22f035be20c76392d4b23126e657956b2fd
Peter McClelland <peterwmcclelland@gmail.com>
2024-01-25 21:05:41 -0600

    icon fade in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
-import './App.scss';
+import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1 className='hero-txt'>
-          TileBar
+        <h1 className="hero-txt">
+          TheShop <span class="material-symbols-outlined">arrow_upward</span>
+          <span className="material-symbols-outlined">arrow_right_alt</span>
+          <span class="material-symbols-outlined">skateboarding</span>
+          <span class="material-symbols-outlined">local_cafe</span>
+          <span class="material-symbols-outlined">autorenew</span>
         </h1>
-        
       </header>
     </div>
   );
