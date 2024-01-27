4c7d0ea9b85c1a8071a6752e1a503b578359fb69
Peter McClelland <peterwmcclelland@gmail.com>
2024-01-26 21:26:51 -0600

    hero transition color

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="hero-txt">
-          TheShop <span class="material-symbols-outlined">arrow_upward</span>
-          <span className="material-symbols-outlined">arrow_right_alt</span>
-          <span class="material-symbols-outlined">skateboarding</span>
-          <span class="material-symbols-outlined">local_cafe</span>
-          <span class="material-symbols-outlined">autorenew</span>
+          TheShop
+          <span class="material-symbols-outlined">arrow_upward</span>
+          <span id="icon-2" className="material-symbols-outlined">
+            arrow_right_alt
+          </span>
+          <span id="icon-3" class="material-symbols-outlined">
+            skateboarding
+          </span>
+          <span id="icon-4" class="material-symbols-outlined">
+            local_cafe
+          </span>
+          <span id="icon-5" class="material-symbols-outlined">
+            autorenew
+          </span>
         </h1>
       </header>
     </div>
