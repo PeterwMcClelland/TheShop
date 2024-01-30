66823b3b677559b8603da9f6ad7972f52205aa9e
Peter McClelland <peterwmcclelland@gmail.com>
2024-01-29 21:13:42 -0600

    sass color update

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="hero-txt">
-          TheShop
-          <span class="material-symbols-outlined">arrow_upward</span>
+          TileBar
           <span id="icon-2" className="material-symbols-outlined">
             arrow_right_alt
           </span>
-          <span id="icon-3" class="material-symbols-outlined">
-            skateboarding
+          <span id="icon-2" className="material-symbols-outlined">
+            arrow_right_alt
+          </span>
+          <span id="icon-2" className="material-symbols-outlined">
+            arrow_right_alt
+          </span>
+          <span id="icon-2" className="material-symbols-outlined">
+            arrow_right_alt
           </span>
           <span id="icon-4" class="material-symbols-outlined">
             local_cafe
-          </span>
-          <span id="icon-5" class="material-symbols-outlined">
-            autorenew
           </span>
         </h1>
       </header>
